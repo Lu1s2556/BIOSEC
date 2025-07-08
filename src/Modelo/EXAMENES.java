9cf8b81bad5dfb5d248f13ae35f6c3870064091c
Lu1s2556 <luiserojasp06@gmail.com>
2025-07-08 12:45:09 -0400

    Creacion de las bases del proyecto

---
+++ src/Modelo/EXAMENES.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author Burrx
+ */
+public class EXAMENES {
+    private int ID_PRUEBA;
+    private String NOMBRE;
+
+    public EXAMENES(int ID_PRUEBA, String NOMBRE) {
+        this.ID_PRUEBA = ID_PRUEBA;
+        this.NOMBRE = NOMBRE;
+    }
+
+    public int getID_PRUEBA() {
+        return ID_PRUEBA;
+    }
+
+    public String getNOMBRE() {
+        return NOMBRE;
+    }
+    
+    
+    
+    
+}
---
+++ src/Utilidades/ConexionBD.java
@@ -0,0 +1,12 @@
+package util;
+import java.sql.*;
+
+public class ConexionBD {
+    private static final String URL = "jdbc:mysql://localhost:3306/BIOSEC";
+    private static final String USER = "root";
+    private static final String PASS = "";
+
+    public static Connection getConexion() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASS);
+    }
+}
